32577ec9f0c9c15dce50d3d0a04bcba94d1a4a9e
Bethany Oestrich <bethany@oestrich.org>
2018-04-14 12:36:04 -0400

    corrected some code throwing errors in create colony

--- app/controllers/colonies_controller.rb
+++ app/controllers/colonies_controller.rb
@@ -71,16 +71,17 @@ class ColoniesController < ApplicationController
     @colony = Colony.find(params[:id])
     @colony.cats.create
     redirect_to(colonies_path)
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_colony
-      @colony = Colony.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_colony
+    @colony = Colony.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def colony_params
-      params.require(:colony).permit(:name, :address)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def colony_params
+    params.require(:colony).permit(:name, :address)
   end
 end
