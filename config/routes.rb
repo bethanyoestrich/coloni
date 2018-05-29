71b47a7aa24f95dd46c25d60cf168b29871f673c
Bethany Oestrich <bethany@oestrich.org>
2018-05-29 13:38:27 -0400

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root 'colonies#index'
+  root 'home#index'
   resources :colonies do
     member do
       post "cat"
   end
 end
+  get "/home", controller: :home, action: :index
 end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
