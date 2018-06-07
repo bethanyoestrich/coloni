fd7476bde7ce1795329ffb5db58dec03c970827e
Bethany Oestrich <bethany@oestrich.org>
2018-06-07 11:40:22 -0400

    added user branch, users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228034544) do
+ActiveRecord::Schema.define(version: 20180607151904) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20180228034544) do
   create_table "colonies", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
