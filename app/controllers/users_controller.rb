dce1f9b7c7aa69cb45a369e14e3e0facae434b6a
Bethany Oestrich <bethany@oestrich.org>
2018-06-14 16:13:33 -0400

    added user index and added user to colony index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def index
+    @users = User.all
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
