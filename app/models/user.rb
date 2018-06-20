6fc129780e38a61c5d461b330dd2bfff56437d14
Bethany Oestrich <bethany@oestrich.org>
2018-06-20 15:45:20 -0400

    added nav bar and footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :colonys
+  has_many :colonies
   validates :username, presence: true,
   uniqueness: {case_sensative: false},
   length: {minimum:3, maximum:25}
